b9c17b7a2d812a4ccf9cfe1ac0163d5b318e5a87
Alexey <slow.net@mail.ru>
2022-10-16 09:39:22 +0300

    Задача с массивами лекция 2

--- Program.cs
+++ Program.cs
@@ -5,14 +5,8 @@ if(arg2>result) result = arg2;
 if(arg3>result) result = arg3;
 return result;
 }
-int a1 = 12;
-int b1 = 22;
-int c1 = 42;
-int a2 = 2551;
-int b2 = 321;
-int c2 = 45;
-int a3 = 18;
-int b3 = 44;
-int c3 = 56;
-int max = Max(Max(a1,b1,c1),Max(a2,b2,c2),Max(a3,b3,c3));
+int[] array = {11,112,31,41,15,61,17,18,19};
+int max = Max(Max(array[0],array[1],array[2]),
+Max(array[3],array[4],array[5])
+,Max(array[6],array[7],array[8]));
 Console.WriteLine(max);

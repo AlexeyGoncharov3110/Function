ef112615d70a6ec62c5c1ea596cfb143d1d29ea4
Alexey <slow.net@mail.ru>
2022-10-16 09:25:00 +0300

    Задача с функциями лекция 2

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int Max (int arg1, int arg2, int arg3)
+{
+int result= arg1;
+if(arg2>result) result = arg2;
+if(arg3>result) result = arg3;
+return result;
+}
+int a1 = 12;
+int b1 = 22;
+int c1 = 42;
+int a2 = 2551;
+int b2 = 321;
+int c2 = 45;
+int a3 = 18;
+int b3 = 44;
+int c3 = 56;
+int max = Max(Max(a1,b1,c1),Max(a2,b2,c2),Max(a3,b3,c3));
+Console.WriteLine(max);
